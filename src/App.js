03630449131a5d4bd30bc3946e36e18decfbb0e4
Shahid Alam <shahid.alam3465@gmail.com>
2022-06-12 04:53:48 +0545

    Navbar component added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import  Navbar  from './Component/Navbar'
+import Body from './Component/Body';
+
+function App() {
+  return (
+    <div><Navbar /><Body /></div>
+  );
+}
+
+export default App;
